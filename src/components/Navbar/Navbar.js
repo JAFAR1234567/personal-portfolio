d7eac874a0ca8a5b337db38cbe5ce13cfac18cbb
Abu Jafar <abmamun437@gmail.com>
2022-06-16 12:53:27 +0600

    navbar and mobile nav responsive done

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import { Link, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
+const Navbar = () => {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+  const location = useLocation();
+  console.log(location);
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+  return (
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button onClick={() => setExpandNavbar((prev) => !prev)}>
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/experience">Experience</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
